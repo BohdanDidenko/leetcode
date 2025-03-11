1495216e3001c1a0eff3de3edb8ee1cb93405660
BohdanDidenko <b.a.didenko@gmail.com>
2025-03-11 20:58:16 +0200

    resolve 2942. Find Words Containing Character task

---
+++ src/test/java/com/leetcode/stack/FindWordsContainingCharacterTest.java
@@ -0,0 +1,61 @@
+package com.leetcode.stack;
+
+import com.leetcode.string.FindWordsContainingCharacter;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindWordsContainingCharacterTest {
+
+    @Test
+    public void testFindWordsContainingCharacter_Example1() {
+        FindWordsContainingCharacter solution = new FindWordsContainingCharacter();
+        String[] words = {"apple", "banana", "cherry"};
+        char x = 'a';
+        List<Integer> expected = List.of(0, 1);
+        List<Integer> actual = solution.findWordsContaining(words, x);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindWordsContainingCharacter_Example2() {
+        FindWordsContainingCharacter solution = new FindWordsContainingCharacter();
+        String[] words = {"hello", "world", "java"};
+        char x = 'o';
+        List<Integer> expected = List.of(0, 1);
+        List<Integer> actual = solution.findWordsContaining(words, x);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindWordsContainingCharacter_NoMatchingWords() {
+        FindWordsContainingCharacter solution = new FindWordsContainingCharacter();
+        String[] words = {"apple", "banana", "cherry"};
+        char x = 'z';
+        List<Integer> expected = List.of();
+        List<Integer> actual = solution.findWordsContaining(words, x);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindWordsContainingCharacter_EmptyArray() {
+        FindWordsContainingCharacter solution = new FindWordsContainingCharacter();
+        String[] words = {};
+        char x = 'a';
+        List<Integer> expected = List.of();
+        List<Integer> actual = solution.findWordsContaining(words, x);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindWordsContainingCharacter_SingleElementArray() {
+        FindWordsContainingCharacter solution = new FindWordsContainingCharacter();
+        String[] words = {"apple"};
+        char x = 'a';
+        List<Integer> expected = List.of(0);
+        List<Integer> actual = solution.findWordsContaining(words, x);
+        assertEquals(expected, actual);
+    }
+}
